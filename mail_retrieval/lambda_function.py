c03a3e4af86a7f25372d6778248d6921bbfaa92f
Burak Bekci <bekciburak97@gmail.com>
2025-11-02 18:59:26 +0100

    Replaces secret manager with SSM (#4)
    * Replaces ssm as secret store
    
    * U.
    
    * U.
    
    * U.

--- mail_retrieval/lambda_function.py
+++ mail_retrieval/lambda_function.py
@@ -9,26 +9,22 @@ from botocore.exceptions import ClientError
 from daily_news import process_mail
 
 
-def get_secret():
-    secret_name = os.environ["SECRET_NAME"]
-    region_name = os.environ["REGION"]
+def get_secret(parameter_key: str):
 
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
+    ssm = boto3.client('ssm')
 
     try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
+        api_key = ssm.get_parameter(
+            Name=parameter_key, 
+            WithDecryption=True
+        )['Parameter']['Value']
+        return api_key
+
     except ClientError as e:
         raise e
-
-    secret_res = json.loads(get_secret_value_response["SecretString"])
-    return secret_res
-
+    
+    return None
+    
 
 def upload_to_bucket(bucket_name: str, file_key:str, content:list):
     s3_client = boto3.client('s3') 
@@ -43,20 +39,18 @@ def upload_to_bucket(bucket_name: str, file_key:str, content:list):
 
 def lambda_handler(event, context):
     load_dotenv()
-    key_result = get_secret()
-
+    
     run_mode = os.environ.get("RUN_MODE", "TEST")
-    bucket_name = os.environ["BUCKET_NAME"]
 
-    parsed_content = process_mail(run_mode, key_result["MAIL_PASS"], key_result["PINECONE_API_KEY"], key_result["GOOGLE_API_KEY"])
+    parsed_content = process_mail(run_mode, get_secret("mail-key"), get_secret("pinecone-key"), get_secret("google-api"))
+
+    bucket_name = os.environ["BUCKET_NAME"]
 
     date_today  = datetime.today()
     key_in_bucket = f"outputs/{date_today.strftime('%Y_%m_%d')}/parsed_news.json"
     upload_success =  upload_to_bucket(bucket_name, key_in_bucket, parsed_content)
 
     return {
-        'statusCode': 200 if upload_success else 500,
-        'body': json.dumps(f'Processing mails finished: {upload_success}')
         'statusCode': 200 if upload_success else 500,
         'body': json.dumps(f'Processing mails finished: {upload_success}')
     }
